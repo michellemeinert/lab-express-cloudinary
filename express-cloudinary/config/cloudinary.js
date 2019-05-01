ca5f7bf9b7712fb0055e301a3f2119b05427b41f
michellemeinert <michelle_meinert@yahoo.de>
2019-05-01 09:28:37 +0200

    broken code

---
+++ express-cloudinary/config/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinaryStorage = require('multer-storage-cloudinary')
+const multer = require('multer');
+const cloudinary = require('cloudinary')
+
+cloudinary.config({
+  cloud_name: 'dbhywmhew',
+  api_key: '187735699843312',
+  api_secret: 'hpgn_Isyyr6Gq9aQAg4_fjmYo8U'
+  });
+  const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "demo",
+  allowedFormats: ["jpg", "png"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }]
+  });
+
+  const parser = multer({ storage: storage });
+
+  module.exports = {
+    cloudinary,
+    multer,
+    cloudinaryStorage,
+    parser
+  }
--- express-cloudinary/routes/posts.js
+++ express-cloudinary/routes/posts.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post')
+// const cloudinaryStorage = require('../config/cloudinary')
+// const cloudinary = require('../config/cloudinary')
+// const mulder = require('../config/cloudinary')
+const parser = require('./../config/cloudinary')
+
 
 
 router.get('/', (req, res, next)=>{
   Post.find({})
-  .then((posts)=> res.render('posts', {posts}))
-  .catch((err)= next(err))
+  .then((posts)=> res.render('posts/index', {posts}))
+  .catch((err)= console.log(err))
+})
+
+router.get('/new', (req, res, next)=>{
+  res.render('posts/new');
+})
+
+router.post('/', parser.single("image") , (req, res, next)=>{
+  const imageUrl = req.file.secure_url;
+  const {title, description} = req.body;
+  const newPost = Post.create({title, description, imageUrl})
+  newPost.save()
+  .then(()=> res.redirect('/posts'))
+  .catch((err)=> res.render('posts/new'))
 })
 
 
